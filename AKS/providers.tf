c88ed3b40bcf2fa5d714939611f097b68e46a0e8
Ganesh-Manoharan18 <ganesh@perfutil.com>
2024-03-07 16:09:09 +0530

    cluster practice

---
+++ AKS/providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "aks-practice"
+    storage_account_name = "akspracticejai"
+    container_name       = "aks-tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
